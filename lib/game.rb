b898176891b07f6c0b6130dced6f9975ef14a95a
Ahmed <ahmed.marzook@cgi.com>
2018-10-19 08:46:07 +0000

    Implemented Skinny controller

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,13 @@
 class Game
-def attack(player)
-  player.receive_damage
-end
+  def attack(player_1, player_2)
+    @players = [player_1, player_2]
+    #player.receive_damage
+  end
+  
+  def player_1
+    @players.first
+  end
+  def player_2
+    @players.last
+  end
 end
